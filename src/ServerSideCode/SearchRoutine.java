bb337e2e4cf0a782c394a128504e31dbdc58bd49
Madhur J. Bajaj <mbajaj.92@gmail.com>
2017-05-30 17:40:40 -0700

    Adding Routine Support (Incomplete)

---
+++ src/ServerSideCode/SearchRoutine.java
@@ -0,0 +1,68 @@
+package ServerSideCode;
+
+import java.util.ArrayList;
+
+import TestCode.Message;
+
+public class SearchRoutine {
+
+	private Object mutex;
+	private static SearchRoutine mSR = null;
+	private ArrayList<QueueObj> mQueue;
+	private SearchRoutineThread mSRThread = null;
+
+	private class QueueObj {
+		String userID;
+		String text;
+	}
+
+	public static SearchRoutine getRoutine() {
+		if (mSR == null)
+			mSR = new SearchRoutine();
+		return mSR;
+	}
+
+	private SearchRoutine() {
+		mQueue = new ArrayList<QueueObj>();
+		mutex = new Object();
+		mSRThread = new SearchRoutineThread();
+		mSRThread.start();
+	}
+
+	public void addSearchRequest(String userId, String query) {
+		QueueObj obj = new QueueObj();
+		obj.userID = userId;
+		obj.text = query;
+		synchronized (mutex) {
+			mQueue.add(obj);
+			mutex.notifyAll();
+		}
+
+		if (mSRThread == null || !mSRThread.isAlive()) {
+			mSRThread = new SearchRoutineThread();
+			mSRThread.start();
+		}
+	}
+
+	private class SearchRoutineThread extends Thread {
+
+		@Override
+		public void run() {
+			while(!mQueue.isEmpty()) {
+				QueueObj obj = null;
+				synchronized (mutex) {
+					obj = mQueue.get(0);
+					mutex.notifyAll();
+				}
+
+				if(obj != null) {
+					Message msg = new Message();
+					msg.links = "";//Utils.getResults(obj.query);
+					msg.msgType = Message.MSG_TYPE.SEARCH;
+					msg.userId = obj.userID;
+					PushNotification.getRoutine().sendMessage(msg);
+				}
+			}
+		}
+	}
+}
--- src/TestCode/Message.java
+++ src/TestCode/Message.java
@@ -4,16 +4,10 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
 	public enum MSG_TYPE {
-		LOGIN, REGISTER_TOKEN, SEARCH, LOGOFF
+		LOGIN, REGISTER_TOKEN, SEARCH, LOGOFF, NOTIFICATION
 	};
 	
 	public MSG_TYPE msgType;
 	private static final long serialVersionUID = 427849097562345L;
-	public String userId, tokens, links, password;
-
-	public Message() {
-		userId = null;
-		tokens = null;
-		links = null;
-	}
+	public String userId, tokens, links, password, users;
 }
