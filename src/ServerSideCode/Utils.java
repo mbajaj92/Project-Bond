809e35b885c95c0bbb4ee0a544d8c4ab4e542a77
Abhidnya Patil <patil.abhidnya@gmail.com>
2017-05-30 17:51:14 -0700

    Adding support for Java-PythonInterface and Retriever

--- src/ServerSideCode/Utils.java
+++ src/ServerSideCode/Utils.java
@@ -1,6 +1,18 @@
 package ServerSideCode;
 
-public class Utils {
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
 
+public class Utils {
 	public static int SERVER_PORT_NUMBER = 2457; 
+	public String returnresults(String query) {
+		
+		PythonInterpreter interpreter = new PythonInterpreter();
+		interpreter.execfile("src/PythonCode/retriever.py");
+		interpreter.set("myquery", query);
+		PyObject linkswithseparatorsnotformatted = interpreter.eval("repr(finddocs(myquery))");
+		String links = linkswithseparatorsnotformatted.toString();
+		links = links.replaceAll("u'|\'$", "");
+		return links;
+	}
 }
